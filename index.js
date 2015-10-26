440cccf87790dc9aeb784684e2d63b4910443137
Alex <abaden22@gmail.com>
2015-10-26 11:19:50 -0400

    updated index.js and changed the package.json repository.url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var crypto  = require("crypto");
 var oauth   = require("oauth");
-var easyxml = require('easyxml');
+var Easyxml = require('easyxml');
 
 var XERO_BASE_URL = 'https://api.xero.com';
 var XERO_API_URL = XERO_BASE_URL + '/api.xro/2.0';
@@ -10,14 +10,12 @@ function Xero (key, secret, rsa_key) {
     this.key = key;
     this.secret = secret;
 
-    easyxml.configure({rootElement: 'Request', manifest: true});
-
     this.oa = new oauth.OAuth(null, null, key, secret, '1.0', null, "PLAINTEXT", null, { "Accept": "application/json" });
     this.oa._signatureMethod = "RSA-SHA1"
     this.oa._createSignature = function (signatureBase, tokenSecret) {
         return crypto.createSign("RSA-SHA1").update(signatureBase).sign(rsa_key, output_format = "base64");
     }
-    
+
     // Attach convenience methods for entities
     require('./src/entities')(this);
 }
@@ -25,9 +23,9 @@ function Xero (key, secret, rsa_key) {
 Xero.prototype.XERO_BASE_URL = XERO_BASE_URL;
 Xero.prototype.XERO_API_URL = XERO_API_URL;
 
-Xero.prototype.call = function (method, path, body, callback) {
+Xero.prototype.call = function (method, path, body, callback, rootElem) {
     var self = this;
-    
+
     if (typeof body === "function" && callback == undefined) {
         callback = body;
         body = null
@@ -35,21 +33,24 @@ Xero.prototype.call = function (method, path, body, callback) {
 
     var xml = null;
     if (method && method !== 'GET' && body) {
-        xml = easyxml.render(body);
+        var rootElement = rootElem || 'Request'
+        var serializer = new Easyxml({rootElement: rootElement, manifest: true, unwrappedArrays: true});
+        xml = serializer.render(body);
     }
     var process = function (err, jsonString, res) {
         if (err) {
             return callback(err);
         }
-        
+
         var json = JSON.parse(jsonString);
-        
+
         if (json.Status !== 'OK') {
           return callback(json, res);
         } else {
           return callback(null, json, res);
         }
     };
+
     return self.oa._performSecureRequest(self.key, self.secret, method, self.XERO_API_URL + path, null, xml, 'application/xml', callback ? process : null);
 };
 
